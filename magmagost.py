b64509f17e6603556a2feabf68477efc887753d3
Ivan Ryabukhin <ryabuchin@gmail.org>
2021-02-23 18:04:02 +0300

    add CLI interface and correct some typos

--- magmagost.py
+++ magmagost.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3
 import argparse
 import random
+import string
 import struct
 import sys
 import csv
 import os
 import typing
 from typing import List, Tuple, BinaryIO, Dict, Union, Optional
+from ansi_colors import Colors
 
 
 class KeyLengthError(RuntimeError):
@@ -31,7 +34,7 @@ def bytes_from_file(filename: str, chunksize: int = 8192):
 class MagmaGost:
     key: int
     sbox: List[List[int]]
-    subkeys: List[int]
+    __subkeys: List[int]
 
     BLOCK_SIZE: int = 64
     KEY_LENGTH: int = 256
@@ -43,7 +46,7 @@ class MagmaGost:
                 'Key must be 256 bits long, current one is %d bit long' % key.bit_length()
             )
         self.key = key
-        self.subkeys = self.expand_key(key)
+        self.__subkeys = self.expand_key(key)
         self.sbox = self.load_sbox_from_csv(sbox_filepath)
 
     @staticmethod
@@ -56,8 +59,10 @@ class MagmaGost:
         return subkeys
 
     def f(self, input: int, key: int):
-        assert input.bit_length() <= 32, \
-            'Bit length of text part must be less than or equal 32, got %d instead' % input.bit_length()
+        if input.bit_length() > 32:
+            raise ValueError(
+                'Bit length of text part must be less than or equal 32, got %d instead' % input.bit_length()
+            )
 
         result = 0
         sum = input ^ key
@@ -82,30 +87,31 @@ class MagmaGost:
         return right ^ self.f(left, round_key), left
 
     def encrypt_bytes(self, byte_buffer: Union[bytes, bytearray]) -> bytes:
-        if len(byte_buffer) != 8:
+        if len(byte_buffer) < 8:
             raise ValueError('Byte buffer must contain eight bytes, got %d instead' % len(byte_buffer))
 
-        right, left = struct.unpack('@2L', byte_buffer)
+        # распаковываем исходные 8 байтов в два unsigned int, по 4 байта каждый
+        right, left = struct.unpack('@2I', byte_buffer)
 
         for i in range(8 * 3):
-            left, right = self.__encryption_round(left, right, self.subkeys[i % 8])
+            left, right = self.__encryption_round(left, right, self.__subkeys[i % 8])
         for i in range(8):
-            left, right = self.__encryption_round(left, right, self.subkeys[7 - i])
+            left, right = self.__encryption_round(left, right, self.__subkeys[7 - i])
 
-        return struct.pack('@LL', right, left)
+        return struct.pack('@2I', right, left)
 
     def decrypt_bytes(self, byte_buffer: Union[bytes, bytearray]) -> bytes:
         if len(byte_buffer) != 8:
             raise ValueError('Byte buffer must contain eight bytes, got %d instead' % len(byte_buffer))
 
-        right, left = struct.unpack('@2L', byte_buffer)
+        right, left = struct.unpack('@2I', byte_buffer)
 
         for i in range(8):
-            left, right = self.__decryption_round(left, right, self.subkeys[i])
+            left, right = self.__decryption_round(left, right, self.__subkeys[i])
         for i in range(8 * 3):
-            left, right = self.__decryption_round(left, right, self.subkeys[(7 - i) % 8])
+            left, right = self.__decryption_round(left, right, self.__subkeys[(7 - i) % 8])
 
-        return struct.pack('@LL', right, left)
+        return struct.pack('@2I', right, left)
 
     def split_into_blocks(self, data: Union[bytes, bytearray]) -> Union[bytes, bytearray]:
         for i in range(0, len(data), self.BLOCK_SIZE // 8):
@@ -120,10 +126,9 @@ class MagmaGost:
             bytes_read = len(data)
             for block in self.split_into_blocks(data):
                 if len(block) < 8:
+                    # "добиваем" блок данных незначащими нулями
                     block = block.ljust(8, b'\x00')
-                f_out.write(
-                    self.encrypt_bytes(block)
-                )
+                f_out.write(self.encrypt_bytes(block))
 
     def encrypt_file(self, infile: str, outfile: str, buffer_size: int = 1024):
         if not os.path.isfile(infile) \
@@ -150,17 +155,11 @@ class MagmaGost:
         while data := f_in.read(buffer_size):
             for block in self.split_into_blocks(data):
                 if len(block) < 8:
+                    # "добиваем" блок данных незначащими нулями
                     block = block.ljust(8, b'\x00')
-                f_out.write(
-                    self.decrypt_bytes(block)
-                )
+                f_out.write(self.decrypt_bytes(block))
 
-    def encrypt(self, plaintext: Union[int, bytes, bytearray]) -> int:
-        """ Шифрование исходного сообщения (открытого текста)
-        :param int plaintext: Открытый текст для зашифрования (64 бита)
-        :return Зашифрованный текст (64 бита)
-        :rtype: int
-        """
+    def encrypt(self, plaintext: int) -> int:
         if plaintext.bit_length() > 64:
             raise RuntimeError(
                 'Size of block must be less than or equal 64 bits, got %d instead' % plaintext.bit_length())
@@ -168,9 +167,9 @@ class MagmaGost:
         left, right = self.split_block(plaintext)
 
         for i in range(8 * 3):
-            left, right = self.__encryption_round(left, right, self.subkeys[i % 8])
+            left, right = self.__encryption_round(left, right, self.__subkeys[i % 8])
         for i in range(8):
-            left, right = self.__encryption_round(left, right, self.subkeys[7 - i])
+            left, right = self.__encryption_round(left, right, self.__subkeys[7 - i])
 
         return (left << (self.BLOCK_SIZE // 2)) | right
 
@@ -182,9 +181,9 @@ class MagmaGost:
         left, right = self.split_block(ciphertext)
 
         for i in range(8):
-            left, right = self.__decryption_round(left, right, self.subkeys[i])
+            left, right = self.__decryption_round(left, right, self.__subkeys[i])
         for i in range(8 * 3):
-            left, right = self.__decryption_round(left, right, self.subkeys[(7 - i) % 8])
+            left, right = self.__decryption_round(left, right, self.__subkeys[(7 - i) % 8])
 
         return (left << (self.BLOCK_SIZE // 2)) | right
 
@@ -203,19 +202,69 @@ class MagmaGost:
 
             # проверка на корректность структуры S-блоков
             if len(data) != 8 or not all([len(row) == 16 for row in data]):
-                raise RuntimeError('The dimensions of the substitution table must be 8x16!')
+                raise ValueError('The dimensions of the substitution table must be 8x16!')
             return data
 
         except IOError as e:
             raise RuntimeError("S-box file %s doesn't exist or isn't readable" % filepath)
 
 
-def main(argv):
-    # Пример
-    # Ключ, указанный в приложении к стандарту ГОСТ 34.12-15 для блочных шифров
-    key = 0xffeeddccbbaa99887766554433221100f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff
+def main():
+    argparser = argparse.ArgumentParser(
+        description='Encrypt/decrypt using "Magma" symmetric block cipher'
+    )
+
+    argparser.add_argument('-k', '--key', dest='key', type=str, metavar='KEY')
+    argparser.add_argument('-i', '--input-file', dest='input', nargs='?', metavar='INFILE',
+        type=argparse.FileType('rb'),
+        default=sys.stdin.buffer
+    )
+
+    argparser.add_argument('-o', '--outfile', nargs='?', metavar='OUTFILE', dest='output',
+        type=argparse.FileType('wb'),
+        default=sys.stdout.buffer
+    )
+
+    argparser.add_argument('-sbox', '--sbox-filepath', required=True, nargs='?', dest='sbox_filepath',
+        type=str, help='path to CSV file with S-box values '
+    )
+
+    action_mode = argparser.add_mutually_exclusive_group(required=True)
+    action_mode.add_argument('-e', '--encrypt', dest='encrypt', help='encrypt file/stdin', action='store_true')
+    action_mode.add_argument('-d', '--decrypt', dest='decrypt', help='decrypt file/stdout', action='store_true')
+
+    argparser.add_argument('--buffer-size', dest='buffer_size', action='store', nargs='?', type=int, default=8*1024)
+
+    args = argparser.parse_args()
+
+    if not args.key:
+        while len(key := input(f'{Colors.BOLD}{Colors.UNDERLINE}Enter key:{Colors.ENDC} ')) != 64:
+            print(Colors.BOLD+Colors.RED + 'Incorrect key length! Must be 64, got', len(key), Colors.ENDC)
+        args.key = key
+    
+    try:
+        args.key = int(args.key, 16)
+    except ValueError:
+        print(Colors.RED + 'Error converting key to integer!' + Colors.ENDC)
+        print(Colors.BOLD + Colors.RED + 'Exiting...' + Colors.ENDC)
+        return 1
+    except OverflowError:
+        print('Overflow occured when converting key to hexadecimal!')
+        print('Exiting...')
+        return 1
+
+    try:
+        magma = MagmaGost(args.key, args.sbox_filepath)
+    except KeyLengthError as e:
+        print(e)
+        return 1
+
+    if args.encrypt:
+        magma.encrypt_stream(args.input, args.output, args.buffer_size)
+    else:
+        magma.decrypt_stream(args.input, args.output, args.buffer_size)
 
     return 0
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    sys.exit(main())
